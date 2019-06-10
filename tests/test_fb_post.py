04ab903d8aa2057f8f7e2c856201e950232eed87
Braian Nestico <bnestico@consumeraffairs.com>
2019-06-10 08:01:01 -0300

    increased coverage

--- tests/test_fb_post.py
+++ tests/test_fb_post.py
@@ -17,9 +17,20 @@ def main():
     assert get_latest_post_message(api) == post_msg, "post was not created"
 
     # Update the last created post
-    updated_msg = "This is a an update to the post!"
+    updated_msg = "This is an update to the post!"
     update_post(api, get_latest_post_id(api), updated_msg)
     assert get_latest_post_message(api) == updated_msg, "post was not updated"
+
+    # Add comment to the post
+    comment_msg = 'Nice post and cool test!'
+    add_post_comment(api, get_latest_post_id(api), comment_msg)
+    print (get_post_comment(api, get_latest_post_id(api)))
+    assert (get_post_comment(api, get_latest_post_id(api))) == comment_msg, "Comment was not added"
+
+    # First, it makes sure the post has no likes and then, it adds a "like" to it
+    assert get_likes_count(api, get_latest_post_id(api)) == 0, "post already has a like"
+    like_post(api, get_latest_post_id(api))
+    assert get_likes_count(api, get_latest_post_id(api)) == 1, "like not added to the post"
 
     # Delete the updated post
     delete_post(api, get_latest_post_id(api))
@@ -57,6 +68,24 @@ def get_latest_post_message(api):
 def get_latest_post_id(api):
     latest_post = api.request(page_id + '/posts?limit=1')
     return latest_post['data'][0]['id']
+
+
+def like_post(api, post_id):
+    api.put_like(post_id)
+
+
+def get_likes_count(api, post_id):
+    likes = api.request(post_id + '?fields=likes.summary(true)')
+    return likes['likes']['summary']['total_count']
+
+
+def add_post_comment(api, post_id, msg):
+    api.put_comment(post_id, msg)
+
+
+def get_post_comment(api, post_id):
+    comment = api.request(post_id + '?fields=comments')
+    return comment['comments']['data'][0]['message']
 
 
 if __name__ == "__main__":

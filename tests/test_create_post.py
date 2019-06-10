52dc74213d59b5b9423f92ede02675a0a3307f12
Braian Nestico <bnestico@consumeraffairs.com>
2019-06-10 06:11:59 -0300

    cleanup

--- tests/test_create_post.py
+++
@@ -1,7 +0,0 @@
-from pages.methods import Methods
-
-
-def test_post():
-    msg = "Hello. world!!!"
-    api = Methods.get_api(config)
-    api.put_object(parent_object="me", connection_name="feed", message=msg)
